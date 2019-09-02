b99a220f1a83f7961835dfc503aa84d32538611c
christian gonçalves lopes <christian_paladino10@hotmail.com>
2019-09-02 01:52:50 -0300

    first commit

---
+++ TesteCrawler/Program.cs
@@ -0,0 +1,88 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TesteCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartCrawler();
+            ExpressaoRegular();
+            Console.ReadLine();
+        }
+
+        private static void ExpressaoRegular()
+        {
+            StreamReader x;
+            string texto = "";
+            string caminho = "C:\\Users\\chris\\source\\repos\\TesteResource\\TesteCrawler\\bin\\Debug\\texto.txt";
+            x = File.OpenText(caminho);
+
+            while (x.EndOfStream != true)
+            {
+                string linha = x.ReadLine();
+                texto = texto + linha + " ";
+            }
+
+            var rgx = new Regex(@"(\d) RESOURCE IT SOLUTIONS [^\}]+?(\d) RESOURCE IT SOLUTIONS");
+            var encontrados = rgx.Matches(texto);
+
+            x.Close();
+            SalvarArquivoTexto(encontrados);
+        }
+
+        private static async Task StartCrawler()
+        {
+            var url = "https://www.valor.com.br/valor-data";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var divs = htmlDocument.DocumentNode.Descendants("div")
+                .Where(n => n.GetAttributeValue("class", "")
+                    .Equals("item")).ToList();
+
+            var cotacaoDolar = divs[0].Descendants("span").FirstOrDefault().InnerText;
+
+            SalvarArquivoTexto(cotacaoDolar);
+        }
+
+        private static void SalvarArquivoTexto(string cotacaoDolar)
+        {
+            StreamWriter x;
+            string caminho = "C:\\Users\\chris\\source\\repos\\TesteResource\\TesteCrawler\\bin\\Debug\\arqCotDollar.txt";
+
+            x = File.CreateText(caminho);
+
+            x.WriteLine(cotacaoDolar);
+            x.Close();
+        }
+
+        private static void SalvarArquivoTexto(MatchCollection encontrados)
+        {
+            StreamWriter x;
+            string caminho = "C:\\Users\\chris\\source\\repos\\TesteResource\\TesteCrawler\\bin\\Debug\\textoRegex.txt";
+
+            x = File.CreateText(caminho);
+
+            foreach (Match item in encontrados)
+            {
+                x.WriteLine(item.Value);
+            }
+
+            x.Close();
+        }
+
+
+    }
+}
---
+++ TesteResource/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TesteResource
+{
+    class Program
+    {
+        class Veiculo
+        {
+            public string Marca { get; set; }
+            public string Modelo { get; set; }
+            public string Ano { get; set; }
+            public double ValorMinimo { get; set; }
+        }
+
+        class Carro : Veiculo
+        {
+            public string Motor { get; set; }
+        }
+
+        class Moto : Veiculo
+        {
+            public string Cilindradas { get; set; }
+        }
+
+        interface IVenda
+        {
+            void VendaVeiculo();
+            void VendaVeiculo(double valorVenda, Veiculo veiculo);
+        }
+
+        class Venda : IVenda
+        {
+            public void VendaVeiculo()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void VendaVeiculo(double valorVenda, Veiculo veiculo)
+            {
+                List<Veiculo> listaVeiculo = new List<Veiculo>(); //imaginando q eu tenha a lista preenchida
+
+                if (valorVenda < veiculo.ValorMinimo)
+                {
+                    var veiculosDisponiveis = listaVeiculo.Where(v => v.ValorMinimo <= valorVenda);
+                    //lista de veiculos disponiveis para o valor de venda
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+        }
+    }
+}
